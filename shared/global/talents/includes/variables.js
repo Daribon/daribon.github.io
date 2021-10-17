46e55b99c4b47fd3a8f28e8ce032aaa5e5cec23e
Daribon <dartarifon@hotmail.com>
2021-10-17 16:33:07 +0200

    Corrected talent layout size for vanilla.

--- shared/global/talents/includes/variables.js
+++ shared/global/talents/includes/variables.js
@@ -1,15 +1,3 @@
-var _____WB$wombat$assign$function_____ = function(name) {return (self._wb_wombat && self._wb_wombat.local_init && self._wb_wombat.local_init(name)) || self[name]; };
-if (!self.__WB_pmw) { self.__WB_pmw = function(obj) { this.__WB_source = obj; return this; } }
-{
-  let window = _____WB$wombat$assign$function_____("window");
-  let self = _____WB$wombat$assign$function_____("self");
-  let document = _____WB$wombat$assign$function_____("document");
-  let location = _____WB$wombat$assign$function_____("location");
-  let top = _____WB$wombat$assign$function_____("top");
-  let parent = _____WB$wombat$assign$function_____("parent");
-  let frames = _____WB$wombat$assign$function_____("frames");
-  let opener = _____WB$wombat$assign$function_____("opener");
-
 var talent = new Array();
 var rank = new Array();
 var tree = new Array();
@@ -30,34 +18,18 @@ var template = new Array();
 
 var levelMax = 60;
 var levelMin = 10;
-var tierNum = 9;
+var tierNum = 7;
 
 var rankPoints = levelMax - levelMin + 1;
 var rankPointsMax = rankPoints;
 var levelCurrent = levelMax;
 
 var theRequiredLevel = levelMin - 1;
-jsLoaded=true;//needed for ajax script loading
-
-}
 /*
-     FILE ARCHIVED ON 01:07:20 Oct 14, 2007 AND RETRIEVED FROM THE
-     INTERNET ARCHIVE ON 17:06:09 Sep 08, 2021.
+     FILE ARCHIVED ON 19:55:24 Aug 21, 2006 AND RETRIEVED FROM THE
+     INTERNET ARCHIVE ON 14:28:57 Oct 17, 2021.
      JAVASCRIPT APPENDED BY WAYBACK MACHINE, COPYRIGHT INTERNET ARCHIVE.
 
      ALL OTHER CONTENT MAY ALSO BE PROTECTED BY COPYRIGHT (17 U.S.C.
      SECTION 108(a)(3)).
-*/
-/*
-playback timings (ms):
-  esindex: 0.017
-  captures_list: 87.232
-  PetaboxLoader3.resolve: 28.925
-  exclusion.robots.policy: 0.208
-  RedisCDXSource: 0.786
-  LoadShardBlock: 61.433 (3)
-  PetaboxLoader3.datanode: 76.644 (4)
-  load_resource: 79.191
-  exclusion.robots: 0.224
-  CDXLines.iter: 20.639 (3)
 */
